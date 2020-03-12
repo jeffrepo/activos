d56e60bf9ffbfdb6517e79a34fae93480d33f5da
Rodrigo Fernandez <rodrigo@solucionesprisma.com>
2020-03-12 17:44:06 +0000

    Nueva forma de dividir los activos

--- models/account_asset.py
+++ models/account_asset.py
@@ -7,11 +7,12 @@ class AccountAssetAsset(models.Model):
 
 #    ubicacion_id = fields.Many2one('stock.location', string='Ubicacion')
     localidad_id = fields.Many2one('activos.localidad', string='Localidad')
+    area_id = fields.Many2one('activos.area', string='Area')
     responsable_id = fields.Many2one('hr.employee', string='Responsable')
     numero_de_serie = fields.Char('Numero de serie')
     marca = fields.Char('Marca')
     modelo = fields.Char('Modelo')
-    etiqueta = fields.Char('Etiqueta')
+    etiqueta = fields.Char('EPC')
     estado = fields.Selection([
         ('bueno', 'Bueno'),
         ('regular', 'Regular'),
--- models/activos.py
+++ models/activos.py
@@ -4,14 +4,12 @@ from odoo import models, fields, api, _
 import logging
 
 
-class Etiquetas(models.Model):
-    _name = 'activos.localidad.etiqueta'
-
-    name = fields.Char("Nombre", required=True)
-
 class Localidad(models.Model):
     _name = 'activos.localidad'
 
     name = fields.Char("Nombre", required=True)
-    etiqueta_ids = fields.Many2many('activos.localidad.etiqueta', 'localidad_etiqueta_rel', 'localidad_id', 'etiqueta_id', string='Etiquetas')
 
+class Area(models.Model):
+    _name = 'activos.area'
+
+    name = fields.Char("Nombre", required=True)
